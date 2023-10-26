5cb1083cf5727c3a8f994bc620e216df903f9529
mhffa-code <moh378407@gmail.com>
2023-10-26 05:03:03 +0200

    new update

--- spot_page/main.js
+++ spot_page/main.js
@@ -66,28 +66,6 @@ sub.addEventListener("click", () => {
     sub_menu.classList.toggle("active");
 });
 
-// email handling (profile section)
-const email = document.querySelector(".email");
-overflowHidden(email, 14);
-// overfliw function 
-function overflowHidden(selector, count)
-{
-    
-
-    let Chars = Array.from(selector.textContent);
-
-    if (window.innerWidth > 991 || window.innerWidth < 667)
-    {
-        Chars.length = count;
-
-        selector.textContent = Chars.join("") + "...";
-    }
-    else if (window.innerWidth > 991 && window.innerWidth < 667)
-    {
-        selector.textContent = Chars.join("");
-    }
-}
-
 // mode
 let pageMoodIcons = document.querySelectorAll(".mood");
 
@@ -99,70 +77,16 @@ pageMoodIcons.forEach((icon) => {
         if (icon.classList.contains("fa-moon"))
         {
             icon.className = "fa-solid fa-sun";
-            logo.src = "open_sesame_night.png";
+            logo.src = "images/open_sesame_night.png";
         }
         else
         {
             icon.className = "fa-solid fa-moon"; 
-            logo.src = "IMG_20231009_172543_034.jpg";
+            logo.src = "images/IMG_20231009_172543_034.jpg";
         }      
         
         document.body.classList.toggle("dark-theme"); 
         
     });
     
-});
-
-// AnnounCements
-const messages = document.querySelectorAll(".AnnounCements .message");
-
-messages.forEach((message) => {
-    if (innerWidth > 1280)
-    {
-        overflowHidden(message, 35);
-    }else if (innerWidth <= 1280 && innerWidth > 1024) {
-        overflowHidden(message, 25);
-    }else if (innerWidth <= 1024 && innerWidth > 992) {
-        overflowHidden(message, 22);
-    }else {
-        overflowHidden(message, 45);
-    }
-});
-// hide with zero balance
-const balances = Array.from(document.querySelectorAll(".acc-balances .balance .num"));
-const hideBox = document.querySelector(".hide-input input");
-
-hideBox.addEventListener("click", () => {
-
-    if (hideBox.checked)
-    {
-        balances.forEach((balance) => {
-            
-          let sequence =  Array.from(balance.textContent);
-          let flag = false;
-          for (let i = 0; i < sequence.length; i++)
-          { 
-                if (sequence[i] != 0)
-                {
-                    if (sequence[i] == '.')
-                        continue;
-                    flag = true;
-                    break;
-                }
-          }
-          if (flag == false)
-          {
-            balance.parentElement.parentElement.parentElement.style.display = "none";
-          }
-          else
-          {
-              flag = false;
-          }
-        });
-    }else {
-        balances.forEach((balance) => {
-            balance.parentElement.parentElement.parentElement.style.display = "flex";
-        });
-    }
-
 });
